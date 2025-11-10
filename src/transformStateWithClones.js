1b1e8292372e41e0c4626026d8e6c37b1fb70ee2
Lucas Pereira <lucasprofissionalp@gmail.com>
2025-11-10 20:22:00 -0300

    Solution

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -1,13 +1,70 @@
 'use strict';
 
 /**
- * @param {Object} state
- * @param {Object[]} actions
- *
- * @return {Object[]}
+ * @param {Object} state Estado inicial
+ * @param {Object[]} actions Lista de ações
+ * @return {Object[]} Lista de estados clonados após cada ação
  */
-function transformStateWithClones(state, actions) {
-  // write code here
+const transformStateWithClones = (state, actions) => {
+  const stateCopy = { ...state };
+  const states = [];
+
+  for (const action of actions) {
+    switch (action.type) {
+      case 'addProperties': {
+        addProperties(stateCopy, action.extraData);
+        break;
+      }
+
+      case 'removeProperties': {
+        removeProperties(stateCopy, action.keysToRemove);
+        break;
+      }
+
+      case 'clear': {
+        clearState(stateCopy);
+        break;
+      }
+
+      default: {
+        break;
+      }
+    }
+
+    states.push(structuredClone(stateCopy));
+  }
+
+  return states;
+};
+
+/**
+ * Adiciona propriedades ao objeto de estado
+ * @param {Object} state Estado atual
+ * @param {Object} data Dados a serem adicionados
+ */
+function addProperties(state, data) {
+  Object.assign(state, data);
+}
+
+/**
+ * Remove propriedades do objeto de estado
+ * @param {Object} state Objeto de estado
+ * @param {string[]} keys Chaves a serem removidas
+ */
+function removeProperties(state, keys) {
+  for (const key of keys) {
+    delete state[key];
+  }
+}
+
+/**
+ * Limpa todas as propriedades do objeto de estado
+ * @param {Object} state Objeto de estado
+ */
+function clearState(state) {
+  for (const key in state) {
+    delete state[key];
+  }
 }
 
 module.exports = transformStateWithClones;
